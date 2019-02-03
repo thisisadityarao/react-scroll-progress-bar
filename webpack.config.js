1bf9ec903814d818cb29481ac8abc24332e39eb6
Aditya Rao <dev.adityarao@gmail.com>
2019-02-03 20:29:49 +0530

    Configure webpack config for npm and update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
   output: {
     path: path.join(__dirname, "build"),
-    filename: "index.js"
+    filename: "index.js",
+    libraryTarget: "commonjs2"
   },
   module: {
     rules: [
@@ -14,7 +15,10 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
-      },
+      }
     ]
   },
+  externals: {
+    react: "commonjs react" // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+  }
 };
