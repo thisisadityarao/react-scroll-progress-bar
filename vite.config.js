8d8c3c3ac8555f2f79bcbbd2bdde1ea3a0942a31
Aditya Rao <dev.adityarao@gmail.com>
2024-05-17 15:32:59 +0530

    Create and write to vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/lib/index.jsx"),
+      name: "React Scroll Progress Bar",
+      fileName: (format) => `react-scroll-progress-bar.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+  plugins: [react()],
+});
